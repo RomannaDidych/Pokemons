87139f852ffefcb7c802cb4da280afe9405415a6
unknown <romanna.d@gmail.com>
2020-03-19 13:00:13 +0200

    tried using async/await functions

--- script.js
+++ script.js
@@ -33,19 +33,21 @@ function loadList(url){
 		for(let i=0; i<arrURL.length; i++){
 		 	arrPromises.push(getServersData(arrURL[i]));		
 		};
-		console.log(arrPromises);
+		//console.log(arrPromises);
 		return Promise.all(arrPromises);	
-	}).then(function(resultArr){		
-		console.log(resultArr)
+	}).then(function(resultArr){ 		
+		//console.log("i'm hear");
+		 console.log(resultArr);
 		// create and return list (ul) one pokemons abilities
-		function getUlAbilities(arr){ 
+		/*function getUlAbilities(arr){ 
 			let abilitiesUl = document.createElement('ul');
 			let arrOnePokemonsAbil = arr.map(arr => arr.ability);			
-			let arrAbilUrl = arrOnePokemonsAbil.map(arrOnePokemonsAbil => [arrOnePokemonsAbil.url]);				
+			let arrAbilUrl = arrOnePokemonsAbil.map(arrOnePokemonsAbil => [arrOnePokemonsAbil.url]);
+			console.log(arrAbilUrl);				
 			for (j=0; j<arrAbilUrl.length; j++){
 				console.log('start iteration '+j);
 				let temp = arrAbilUrl[j];
-				let shortAbilArr = getServersData(temp).then(function(val){
+				let shortAbilArr = getServersData(temp).then(function(val){ 
 					console.log('entered then '+j);					
 					let abilityText = val.name + ":     " + val.effect_entries[0]["short_effect"] + ";";				
 					let abilityLi = document.createElement('li');
@@ -58,9 +60,43 @@ function loadList(url){
 			};
 			console.log('completed function');
 			return abilitiesUl;		
+		};*/
+
+		function getUlAbilities(arr){
+			let abilitiesUl = document.createElement('ul');
+			let arrOnePokemonsAbil = arr.map(arr => arr.ability);			
+			let arrAbilUrl = arrOnePokemonsAbil.map(arrOnePokemonsAbil => [arrOnePokemonsAbil.url]);
+			//console.log(arrAbilUrl);
+			async function asyncLoop(array){
+				//console.log("async function started");
+				function createUl(val){
+						let abilityText = val.name + ":     " + val.effect_entries[0]["short_effect"] + ";";				
+						let abilityLi = document.createElement('li');
+						let text = document.createTextNode(abilityText);
+						abilityLi.append(text);
+						abilitiesUl.append(abilityLi);
+					};
+
+				let iteration = 0;
+				for (let item of array){
+					iteration++;
+					console.log("started iteration " + iteration)
+					let value = await getServersData(item);
+					console.log("got data " + iteration);
+					await createUl(value);
+					console.log("ended iteration " + iteration)
+				};				
+			};
+			asyncLoop(arrAbilUrl);
+			console.log("async function finished");
+			return abilitiesUl;		
 		};
 
-		let arrAbilities = resultArr.map(resultArr => resultArr.abilities);		
+
+			
+
+
+		let arrAbilities = resultArr.map(resultArr => resultArr.abilities);			
 		let divList = document.createElement('div');
 		divList.id = currentID;		 
 		for (let i=0; i<arrAbilities.length; i++){
@@ -70,6 +106,7 @@ function loadList(url){
 			newLi.append(h2);			
 			divList.append(newLi);			
 			let newUl = getUlAbilities(arrAbilities[i]);
+			console.log(newUl);			
 			newLi.append(newUl);			
 		}; 
 		maxPagesNumber +=1;
